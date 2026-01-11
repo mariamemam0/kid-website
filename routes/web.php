095bd01e66edca2bce8484ac5310da2bddab6e07
mariam <mariamemampp00@gmail.com>
2026-01-11 15:45:04 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -32,3 +32,4 @@ Route::post('/email/verification-notification',function(Request $request){
 $request->user()->sendEmailVerificationNotification();
 return back()->with('message','Verification email sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
+//
