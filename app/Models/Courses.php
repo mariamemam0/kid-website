deb92b10d7cc8a70a5b7b6de97576e00f300b931
mariam <mariamemampp00@gmail.com>
2026-01-18 16:15:48 +0200

    courses data

--- app/Models/Courses.php
+++ app/Models/Courses.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Courses extends Model
 {
     
+protected $fillable = [
+    'name',
+    'description',
+    'age_from',
+    'age_to',
+    'capacity',
+    'start_time',
+    'end_time',
+    'price',
+];
 
      public function kids()
      {
